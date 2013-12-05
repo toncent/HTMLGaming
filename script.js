f2e690951bf63f913e398a4300e9c91c6f8ebc6a
unknown <vincentberlin@me.com>
2013-12-05 17:01:55 +0100

    switched to canvas drawing

--- script.js
+++ script.js
@@ -20,6 +20,8 @@
 	var gameStarted = 0;
 	var gameVisible = 0;
 	var gameFps = 60;
+	var canvas;
+	var timerStatus;
 
 /*~~~~~~~~~~Event Listener~~~~~~~~~~*/
 	document.addEventListener('DOMContentLoaded', initListeners)
@@ -75,79 +77,145 @@
 /*~~~~~~~~~~Funktionen~~~~~~~~~~*/
 
 	function init(){
-		document.getElementById("t").innerHTML = "";
-		document.getElementById("t").style.left = "385px";
+		canvas = document.getElementById("canvas");
 		/*-----FPS-----*/
 		window.setInterval("update()",1000/gameFps);
 	}
 
-
-	function update(){
-		
-		/*------Paddelbewegung------*/
+	function paddleMove(){
 		if ((rightV > 0 && rightPaddlePos<450)||(rightV<0 && rightPaddlePos>0)) {
 			rightPaddlePos += rightV;
 		};
 		if ((leftV > 0 && leftPaddlePos<450)||(leftV<0 && leftPaddlePos>0)) {
 			leftPaddlePos += leftV;
 		};
-		if (paused == 1) {
-			pauseTimer -= 16;
-			document.getElementById("t").innerHTML = Math.ceil((pauseTimer*3)/2000);
-			if (pauseTimer <= 0) {
-				paused = 0;
-				pauseTimer = 2000;
-				document.getElementById("t").style.display = "none";
-			}
-		} else {
-			/*------Kollisionsabfragen------*/
-			//Ball trifft Paddel (rechts)
-			if (ballPosX>=750 && ballPosX<=780 && rightPaddlePos<(ballPosY+9) && (rightPaddlePos+150)>ballPosY) {
-				ballVtotal += 0.3;
-				ballVy = ((ballPosY - rightPaddlePos)/150)*1.6*ballVtotal-0.8*ballVtotal; //<--kompliziert
-				ballVx = -1*Math.sqrt(ballVtotal*ballVtotal - ballVy*ballVy);
-				updateBallColor();
-			};
+	}
 
-			//Ball trifft Paddel (links)
-			if (ballPosX<=40 && ballPosX>=10 && leftPaddlePos<(ballPosY+9) && (leftPaddlePos+150)>ballPosY) {
-				ballVtotal += 0.3;
-				ballVy = ballVy = ((ballPosY - leftPaddlePos)/150)*1.6*ballVtotal-0.8*ballVtotal;
-				ballVx = Math.sqrt(ballVtotal*ballVtotal - ballVy*ballVy);
-				updateBallColor();
-			};
+	function manageTimer(){
+		pauseTimer -= 1000/gameFps;
+		timerStatus = Math.ceil((pauseTimer*3)/2000);
+		if (pauseTimer <= 0) {
+			paused = 0;
+			pauseTimer = 2000;
+		}
+	}
 
-			//Ball trifft obere oder untere Wand
-			if (ballPosY>=590 || ballPosY<=0) {
-				ballVy *= -1;
-				ballVtotal +=0.1;
-				updateBallColor();
-			}
-
-			//Ball geht ins Aus (links)
-			if(ballPosX<=0){
-				reset();
-				rightScore += 1;
-				document.getElementById("rs").innerHTML = rightScore;
-			
-			//Ball geht ins Aus (rechts)
-			} else if(ballPosX>=790){
-				reset();
-				leftScore += 1;
-				document.getElementById("ls").innerHTML = leftScore;
-
-			/*------Ballbewegung------*/
-			} else {
-				ballPosX += ballVx;
-			};
-			ballPosY += ballVy; 
+	function manageCollisions(){
+		//Ball trifft Paddel (rechts)
+		if (ballPosX>=750 && ballPosX<=780 && rightPaddlePos<(ballPosY+9) && (rightPaddlePos+150)>ballPosY) {
+			ballVtotal += 0.3;
+			ballVy = ((ballPosY - rightPaddlePos)/150)*1.6*ballVtotal-0.8*ballVtotal; //<--kompliziert
+			ballVx = -1*Math.sqrt(ballVtotal*ballVtotal - ballVy*ballVy);
+			updateBallColor();
 		}
 
+		//Ball trifft Paddel (links)
+		if (ballPosX<=40 && ballPosX>=10 && leftPaddlePos<(ballPosY+9) && (leftPaddlePos+150)>ballPosY) {
+			ballVtotal += 0.3;
+			ballVy = ballVy = ((ballPosY - leftPaddlePos)/150)*1.6*ballVtotal-0.8*ballVtotal;
+			ballVx = Math.sqrt(ballVtotal*ballVtotal - ballVy*ballVy);
+			updateBallColor();
+		}
+
+		//Ball trifft obere oder untere Wand
+		if (ballPosY>=590 || ballPosY<=0) {
+			ballVy *= -1;
+			ballVtotal +=0.1;
+			updateBallColor();
+		};
+	}
+
+	function draw(){
+		var buffer = document.createElement("canvas");
+		buffer.width = canvas.width;
+		buffer.height = canvas.height;
+		var buffer_ctx = buffer.getContext("2d");
+		buffer_ctx.fillStyle = "#000000";
+		buffer_ctx.fillRect(0,0,800,600);
+		buffer_ctx.fillStyle ="#FFFFFF";
+		buffer_ctx.strokeStyle ="#FFFFFF";
+
+		if(paused==1){
+			drawTimer();
+		}
+		drawPaddle(20, leftPaddlePos, buffer_ctx);
+		drawPaddle(760, rightPaddlePos, buffer_ctx);
+		drawScore(buffer_ctx);
+		drawBall(buffer_ctx);
+		
+		var ctx = canvas.getContext("2d");
+		ctx.fillStyle ="black";
+		ctx.fill();
+		ctx.drawImage(buffer,0,0);
+		/*document.getElementById("lp").style.top = leftPaddlePos;
+		document.getElementById("rp").style.top = rightPaddlePos;
+		document.getElementById("b").style.top = ballPosY;
+		document.getElementById("b").style.left = ballPosX;
+		document.getElementById("rs").innerHTML = rightScore;	
+		document.getElementById("ls").innerHTML = leftScore;*/
+	}
+
+	function drawPaddle( paddleX, paddleY, buffer_ctx){
+		buffer_ctx.beginPath();
+		buffer_ctx.lineWidth=10;
+		buffer_ctx.lineJoin="round";
+		buffer_ctx.moveTo(paddleX+5,paddleY+5);
+		buffer_ctx.lineTo(paddleX+15,paddleY+5);
+		buffer_ctx.lineTo(paddleX+15,paddleY+145);
+		buffer_ctx.lineTo(paddleX+5,paddleY+145);
+		buffer_ctx.lineTo(paddleX+5,paddleY+5);
+		buffer_ctx.lineTo(paddleX+10,paddleY+5);
+		buffer_ctx.stroke();
+	}
+
+	function drawBall(buffer_ctx){
+		buffer_ctx.beginPath();
+		buffer_ctx.arc(ballPosX,ballPosY,5,0,2*Math.PI);
+		buffer_ctx.fill();
+	}
+
+	function drawTimer(buffer_ctx){
+
+	}
+
+	function drawScore(){
+
+	}
+
+	function manageBall(){
+		//Ball geht ins Aus (links)
+		if(ballPosX<=0){
+			reset();
+			rightScore += 1;
+		
+		//Ball geht ins Aus (rechts)
+		} else if(ballPosX>=790){
+			reset();
+			leftScore += 1;
+
+		//Ballbewegung
+		} else {
+			ballPosX += ballVx;
+		};
+		ballPosY += ballVy;
+	}
+
+	function update(){
+		
+		/*------Paddelbewegung------*/
+		paddleMove();
+
+		if (paused == 1) {
+			manageTimer();
+		} else {
+			/*------Kollisionsabfragen------*/
+			manageCollisions();
+
+			/*-------Ball----------------*/
+ 			manageBall();
+		}
 			/*------Positionen updaten------*/
-			document.getElementById("lp").style.top = leftPaddlePos;
-			document.getElementById("rp").style.top = rightPaddlePos;
-			document.getElementById("b").style.top = ballPosY;
-			document.getElementById("b").style.left = ballPosX;	
+			draw();
 	}
 
 	function updateBallColor(){
@@ -164,7 +232,7 @@
 			result += "0";
 		};
 		result += blue.toString(16);
-		document.getElementById("b").style.backgroundColor = "#"+result;
+		//document.getElementById("b").style.backgroundColor = "#"+result;
 	}
 
 	function reset(){
@@ -178,5 +246,4 @@
 		ballVtotal = 7;
 		updateBallColor();
 		paused = 1;
-		document.getElementById("t").style.display = "block";
 	}
