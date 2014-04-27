1fd564be840ea78dc1c663e46c8f04f2f70bf553
unknown <vincentberlin@me.com>
2014-04-27 20:27:40 +0200

    andere steuerung und aktives teil leuchtet auf

--- tileturner/script.js
+++ tileturner/script.js
@@ -9,7 +9,7 @@ function init() {
 	var gameDiv = document.getElementById("gameDiv");
 	for(var j=0; j<rowCount; j++) {
 		for (var i = 0; i < colCount; i++) {
-			gameDiv.innerHTML += "<div class='img-container'><img src='" + imgPath + "' style='top:" + -1*j*(600/rowCount) + "px; left:" + -1*i*(1000/colCount) + "px;'></div>"
+			gameDiv.innerHTML += "<div class='img-container' style='top:"+(10+j*200)+"px; left:"+(10+i*200)+"px;'><img src='" + imgPath + "' style='top:" + -1*j*(600/rowCount) + "px; left:" + -1*i*(1000/colCount) + "px;'></div>"
 		};
 	}
 	for(var j=0; j<colCount; j++) {
@@ -22,29 +22,49 @@ function init() {
 		keyDownHandler(e.keyCode);
 	});
 	document.getElementById("startButton").addEventListener("click", scramble);
+	updateGlow(0);
 }
 
 function keyDownHandler(key) {
-	if (key == 37) {
-		rotatePanel(-1);
+	if (key == 32) {
+		rotatePanel();
+	} else	if (key == 37) {
+		if (activePanel>0) {
+			updateGlow(-1);
+			activePanel--;
+		}
 	} else if (key == 39) {
-		rotatePanel(1);
-	} else if (key == 38) {
-		if(activePanel<(rowCount*colCount)-1){
+		if (activePanel<(colCount*rowCount)-1) {
+			updateGlow(1);
 			activePanel++;
 		}
+	} else if (key == 38) {
+		if(activePanel>=colCount){
+			updateGlow(-1*colCount);
+			activePanel -= colCount;
+		}
 	} else if (key == 40) {
-		if (activePanel>=1) {
-			activePanel--;
+		if (activePanel<(rowCount*colCount)-colCount) {
+			updateGlow(colCount);
+			activePanel+= colCount;
 		};
 	};
 }
 
-function rotatePanel(direction) {
+function updateGlow(change){
+	document.getElementsByClassName("img-container")[activePanel].style.boxShadow = "none";
+	document.getElementsByClassName("img-container")[activePanel].style.webkitBoxShadow = "none";
+	document.getElementsByClassName("img-container")[activePanel].style.zIndex = "0";
+	document.getElementsByClassName("img-container")[activePanel+change].style.boxShadow = "0 0 30px yellow";
+	document.getElementsByClassName("img-container")[activePanel+change].style.webkitBoxShadow = "0 0 30px yellow";
+	document.getElementsByClassName("img-container")[activePanel+change].style.zIndex = "1";
+}
+
+function rotatePanel() {
 	var currentRow = getRow(activePanel);
 	var currentCol = getColumn(activePanel);
 
-	panelRotations[currentCol][currentRow] += direction*90;
+	panelRotations[currentCol][currentRow] += 90;
 	document.getElementsByClassName("img-container")[activePanel].style.transform = "rotate("+ panelRotations[currentCol][currentRow] +"deg)";
 	document.getElementsByClassName("img-container")[activePanel].style.webkitTransform = "rotate("+ panelRotations[currentCol][currentRow] +"deg)";	
 }
